52910cef863a2ba673c1aeebe415dd11fe9cdd87
salaudeen <olayiwolasalaudeen@gmail.com>
2024-01-09 18:35:41 +0100

    creation of star rating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -13,7 +13,8 @@ const textStyle = {
   margin: "0",
 };
 export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [temRating, setTemRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -42,7 +43,13 @@ const starStyle = {
 };
 function Star({ onRate, full }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={() => console.log("Enter")}
+      onMouseLeave={() => console.log("Leave")}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
